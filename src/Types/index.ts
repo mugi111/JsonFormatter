977d9b8801ff99e304508213eb7fb432c92a21e1
mugi <m.111_t.1230_nittc@outlook.com>
2020-11-02 06:39:39 +0900

    add atoms

--- src/Types/index.ts
+++ src/Types/index.ts
@@ -13,3 +13,13 @@ export interface IModelsList {
   id: number;
   contents: IModelsElem[];
 }
+
+export interface IObjectsElem {
+  model: IModelsElem;
+  value: string | number | boolean;
+}
+
+export interface IObjectsList {
+  id: number;
+  contents: IObjectsElem[];
+}
