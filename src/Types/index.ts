be3970739d477e231c70b07e1243f5a3875061c5
mugi <m.111_t.1230_nittc@outlook.com>
2020-11-01 20:28:02 +0900

    add models list

--- src/Types/index.ts
+++ src/Types/index.ts
@@ -3,3 +3,13 @@ export class InputTypes {
   static readonly Number = "Number";
   static readonly Boolean = "Boolean";
 }
+
+export interface IModelsElem {
+  Key: string;
+  type: InputTypes;
+}
+
+export interface IModelsList {
+  id: string;
+  contents: IModelsElem[];
+}
