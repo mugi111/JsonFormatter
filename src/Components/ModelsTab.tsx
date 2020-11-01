6e7a97d5d92bf659c6a61c09adbf72a26af87a94
mugi <m.111_t.1230_nittc@outlook.com>
2020-11-01 20:54:35 +0900

    setup recoil

--- src/Components/ModelsTab.tsx
+++ src/Components/ModelsTab.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 import { Button } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
 import { ModelsForm } from './ModelsForm';
+import { useRecoilState } from 'recoil';
+import { modelsListState } from '../Recoil/atom';
 import '../Styles/models-tab.scss';
 
 export const ModelsTab: React.FC = () => {
+  const [modelsList, setModelsList] = useRecoilState(modelsListState)
+
+  const addFormHandler = () => {
+
+  }
+
   return (
     <div>
-      <ModelsForm></ModelsForm>
-      <Button className="models-tab__button">
+      {modelsList.map((_, i) => <ModelsForm></ModelsForm>)}
+      <Button className="models-tab__button" onClick={addFormHandler}>
         <PlusOutlined />
       </Button>
     </div>
