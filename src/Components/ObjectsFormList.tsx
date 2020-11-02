a5653e75b46cf7af50295ebf687b1b35b0e7cb59
Takumi Hirunuma <m.111_t.1230_nittc@outlook.com>
2020-11-02 16:55:28 +0900

    add arraysize

--- src/Components/ObjectsFormList.tsx
+++ src/Components/ObjectsFormList.tsx
@@ -28,13 +28,32 @@ export const ObjectsFormList: React.FC<Props> = (props: Props) => {
       const model = modelsList.find((e) => e.id === v);
       let object: IObjectsElem[] = [];
       model?.contents.map((e) => {
-        object.push({ key: e.key, type: e.type, isArray: e.isArray, value: [] });
+        object.push({ key: e.key, type: e.type, isArray: e.isArray, value: [""] });
         return true;
       })
       return prev.slice(0, props.formIndex).concat({ id: prev[props.formIndex].id, model: object }).concat(prev.slice(props.formIndex + 1, prev.length));
     })
   }
 
+  const changeArraySize = (v: string | number | undefined, i: number) => {
+    setObjectsList((prev) => {
+      if (Number(v) === prev[props.formIndex].model[i].value.length) {
+        return prev;
+      } else if (Number(v) < prev[props.formIndex].model[i].value.length) {
+        const prevModel = prev[props.formIndex].model[i];
+        return prev.slice(0, props.formIndex).concat({
+          id: prev[props.formIndex].id, model: prev[props.formIndex].model.slice(0, i).concat({ key: prevModel.key, type: prevModel.type, isArray: prevModel.isArray, value: prevModel.value.slice(0, Number(v)) })
+        }).concat(prev.slice(props.formIndex + 1, prev.length));
+      } else {
+        const prevModel = prev[props.formIndex].model[i];
+        let addObj: Array<string | number | boolean> = Array<string | number | boolean>();
+        [...Array(Number(v) - prev[props.formIndex].model[i].value.length)].forEach((_) => addObj.push(""));
+        return prev.slice(0, props.formIndex).concat({
+          id: prev[props.formIndex].id, model: prev[props.formIndex].model.slice(0, i).concat({ key: prevModel.key, type: prevModel.type, isArray: prevModel.isArray, value: prevModel.value.concat(addObj) }).concat(prev[props.formIndex].model.slice(i + 1, prevModel.value.length))
+        }).concat(prev.slice(props.formIndex + 1, prev.length));
+      }
+    });
+  }
 
   return (
     <div className="models-tab">
@@ -48,7 +67,19 @@ export const ObjectsFormList: React.FC<Props> = (props: Props) => {
       </Select>
 
       {objectsList[props.formIndex].model.map((e, i) => {
-        console.log(e.type);
+        const ArraySize: React.FC = () => {
+          if (e.isArray) {
+            return (
+              <>
+                <label>ArraySize</label>
+                <InputNumber placeholder="Array Size" value={e.value.length} onChange={(v) => changeArraySize(v, i)}></InputNumber>
+              </>
+            );
+          } else {
+            return <></>;
+          }
+        }
+
         const InputField: React.FC = () => {
           if (e.type === InputTypes.string) {
             return <Input placeholder="value"></Input>;
@@ -63,8 +94,16 @@ export const ObjectsFormList: React.FC<Props> = (props: Props) => {
 
         return (
           <div>
-            <label>{e.key} : </label>
-            <InputField></InputField>
+            <ArraySize></ArraySize>
+            <p>{e.value.length}</p>
+            {e.value.map((_) => {
+              return (
+                <>
+                  <label>{e.key} : </label>
+                  <InputField></InputField>
+                </>
+              );
+            })}
           </div>
         )
       }
