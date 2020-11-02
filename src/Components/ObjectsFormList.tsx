e6ccff745ddfd722d4b69f50f7bec258a123bc0b
Takumi Hirunuma <m.111_t.1230_nittc@outlook.com>
2020-11-02 14:17:29 +0900

    add add objectform event

--- src/Components/ObjectsFormList.tsx
+++ src/Components/ObjectsFormList.tsx
@@ -5,6 +5,8 @@ import { ModelsForm } from './ModelsForm';
 import { useRecoilState } from 'recoil';
 import { objectsListState } from '../Recoil/atom';
 import '../Styles/models-tab.scss';
+import { InputTypes } from '../Types';
+import { ObjectsForm } from './ObjectsForm';
 
 interface Props {
   formIndex: number;
@@ -13,16 +15,22 @@ interface Props {
 export const ObjectsFormList: React.FC<Props> = (props: Props) => {
   const [objectsList, setObjectsList] = useRecoilState(objectsListState);
 
+  const addObjectsFormList = () => {
+    setObjectsList((prev) => {
+      return prev.slice(0, props.formIndex).concat([{ id: prev[props.formIndex].id, contents: [...prev[props.formIndex].contents, { model: { key: "", type: InputTypes.string, isArray: false }, value: "" }] }]).concat(prev.slice((props.formIndex + 1), prev.length));
+    })
+  }
+
   return (
     <div className="models-tab">
       <h3>{objectsList[props.formIndex].id}</h3>
       {objectsList[props.formIndex].contents.map((_, i) => {
         return (
-          <ModelsForm formIndex={props.formIndex} modelIndex={i}></ModelsForm>
+          <ObjectsForm formIndex={props.formIndex} objectIndex={i}></ObjectsForm>
         )
       }
       )}
-      <Button className="models-tab__button">
+      <Button className="models-tab__button" onClick={addObjectsFormList}>
         <PlusOutlined />
       </Button>
       <Button danger className="models-form__button">
