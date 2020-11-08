24e87e000541b84a5296b23adba8917179118a39
mugi <m.111_t.1230_nittc@outlook.com>
2020-11-09 00:08:25 +0900

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,33 @@
 import React from 'react';
-import './App.css';
 import { Tabs } from 'antd';
 import { ModelsTab } from './Components/ModelsTab';
 import { ObjectsTab } from './Components/ObjectsTab';
 import { OutputTab } from './Components/OutputTab';
+import './App.css';
 import 'antd/dist/antd.css';
+import './Styles/header.scss';
 
 const { TabPane } = Tabs;
 
 function App() {
   return (
     <div className="App">
-      <Tabs defaultActiveKey="1">
-        <TabPane tab="Models" key="1">
-          <ModelsTab></ModelsTab>
-        </TabPane>
-        <TabPane tab="Objects" key="2">
-          <ObjectsTab></ObjectsTab>
-        </TabPane>
-        <TabPane tab="Output" key="3">
-          <OutputTab></OutputTab>
-        </TabPane>
-      </Tabs>
+      <header className="header">
+        <h1 className="header__h1">Json Formmatter</h1>
+      </header>
+      <div className="body">
+        <Tabs defaultActiveKey="1">
+          <TabPane tab="Models" key="1">
+            <ModelsTab></ModelsTab>
+          </TabPane>
+          <TabPane tab="Objects" key="2">
+            <ObjectsTab></ObjectsTab>
+          </TabPane>
+          <TabPane tab="Output" key="3">
+            <OutputTab></OutputTab>
+          </TabPane>
+        </Tabs>
+      </div>
     </div>
   );
 }
